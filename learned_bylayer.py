d1090137b016c834aae3b36f521fdbc5e95642e5
Tomerporian <57691643+Tomerporian@users.noreply.github.com>
2023-08-09 11:51:00 +0300

    Update learned_bylayer.py

--- learned_bylayer.py
+++ learned_bylayer.py
@@ -1,34 +1,12 @@
-###
-# Disclaimer: This is not our central method, we recommend the greedy soup which is found in main.py.
-# This method is described in appendix I and, compared to main.py, this code is much less tested.
-# For instance, we don't know how stable the results are under optimization noise. However, we expect
-# this method to outperform greedy soup. Still, we recommend using greedy soup and not this. 
-# As mentioned in the paper, this code is computationally expernsive as it requires loading models in memory.
-# We run this on a node with 490GB RAM and use 1 GPU with 40GB of memory.
-# It also looks like PyTorch released a very helpful utility which we recommend if re-implementing:
-# https://pytorch.org/docs/stable/generated/torch.nn.utils.stateless.functional_call.html?utm_source=twitter&utm_medium=organic_social&utm_campaign=docs&utm_content=functional-api-for-modules
-# When running with lr = 0.05 and epochs = 5 we get 81.38%.
-###
-
 import argparse
 import os
-import wget
-import torch
-import clip
-import os
 import time
+import logging
 
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 
 import torch
-from torch.utils.data import SubsetRandomSampler
-from torchvision import models
-from torch.autograd.functional import vhp, jvp, jacobian
-from torchvision import datasets
-
-from datasets.imagenet import ImageNet2pShuffled, ImageNet
+from timm import create_model
+from timm.data import create_dataset, create_loader, resolve_data_config
 
 from utils import ModelWrapper, maybe_dictionarize_batch, cosine_lr
 
@@ -37,17 +15,14 @@ def parse_arguments():
     parser.add_argument(
         "--data-location",
         type=str,
-        default=os.path.expanduser('~/data'),
+        default=os.path.expanduser('/a/home/cc/students/cs/tomerporian/timm_averaging/data'),
         help="The root directory for the datasets.",
     )
     parser.add_argument(
         "--model-location",
         type=str,
-        default=os.path.expanduser('~/ssd/checkpoints/soups'),
-        help="Where to download the models.",
-    )
-    parser.add_argument(
-        "--download-models", action="store_true", default=False,
+        default=os.path.expanduser('/a/home/cc/students/cs/tomerporian/timm_averaging/results_imgnet_avg/23-08-03-ImageNet-rsn50-wd-and-coeffs-compute/002_23-08-03-ImageNet-rsn50-wd-and-coeffs-compute+lr=0.24+wei_dec=3.33e-05'),
+        help="Where the models are.",
     )
     parser.add_argument(
         "--batch-size",
@@ -93,28 +68,28 @@ class AlphaWrapper(torch.nn.Module):
         self.paramslist = paramslist
         self.model = model
         self.names = names
-        ralpha = torch.ones(len(paramslist[0]), len(paramslist))
-        ralpha = torch.nn.functional.softmax(ralpha, dim=1)
+        ralpha = torch.ones(len(paramslist))
+        ralpha = torch.nn.functional.softmax(ralpha)
         self.alpha_raw = torch.nn.Parameter(ralpha)
         self.beta = torch.nn.Parameter(torch.tensor(1.))
 
     def alpha(self):
-        return torch.nn.functional.softmax(self.alpha_raw, dim=1)
+        return torch.nn.functional.softmax(self.alpha_raw)
 
     def forward(self, inp):
         alph = self.alpha()
-        params = tuple(sum(tuple(pi * alphai for pi, alphai in zip(p, alph[j].cpu()))) for j, p in enumerate(zip(*self.paramslist)))
+        params = tuple(sum(alph[i] * p for i, p in enumerate(paramslist)))
         params = tuple(p.cuda(0) for p in params)
         load_weights(self.model, self.names, params)
         out = self.model(inp)
         return self.beta * out
 
-def get_imagenet_acc(test_dset):
+def get_imagenet_acc(loader_test):
     with torch.no_grad():
         correct = 0.
         n = 0
         end = time.time()
-        for i, batch in enumerate(test_dset.test_loader):
+        for i, batch in enumerate(loader_test):
             batch = maybe_dictionarize_batch(batch)
             inputs, labels = batch['images'].cuda(), batch['labels'].cuda()
             data_time = time.time() - end
@@ -127,10 +102,10 @@ def get_imagenet_acc(test_dset):
             n += y.size(0)
 
             batch_time = time.time() - end
-            percent_complete = 100.0 * i / len(test_dset.test_loader)
+            percent_complete = 100.0 * i / len(loader_test)
             if ( i % 10 ) == 0:
                 print(
-                    f"Train Epoch: {0} [{percent_complete:.0f}% {i}/{len(test_dset.test_loader)}]\t"
+                    f"Train Epoch: {0} [{percent_complete:.0f}% {i}/{len(loader_test)}]\t"
                     f"Loss: {loss.item():.6f}\tData (t) {data_time:.3f}\tBatch (t) {batch_time:.3f}", flush=True
                 )
             end = time.time()
@@ -140,37 +115,66 @@ def get_imagenet_acc(test_dset):
 
 
 if __name__ == '__main__':
+    print("parse arguments...")
     args = parse_arguments()
-    NUM_MODELS = 72
+    NUM_MODELS = 250
 
-    # Step 1: Download models.
-    if args.download_models:
-        if not os.path.exists(args.model_location):
-            os.mkdir(args.model_location)
-        for i in range(NUM_MODELS):
-            print(f'\nDownloading model {i} of {NUM_MODELS - 1}')
-            wget.download(
-                f'https://github.com/mlfoundations/model-soups/releases/download/v0.0.2/model_{i}.pt',
-                out=args.model_location
-                )
-
-    model_paths = [os.path.join(args.model_location, f'model_{i}.pt') for i in range(NUM_MODELS)]
-    base_model, preprocess = clip.load('ViT-B/32', 'cpu', jit=False)
+    model_paths = [os.path.join(args.model_location, f'checkpoint-{i}.pt') for i in range(NUM_MODELS)]
+    print("Creating base model...")
+    base_model = create_model('resnet50', pretrained=False)
     criterion = torch.nn.CrossEntropyLoss()
     device = torch.device('cuda')
-    train_dset = ImageNet2pShuffled(preprocess, location=args.data_location, batch_size=args.batch_size, num_workers=args.workers)
-    test_dset = ImageNet(preprocess, location=args.data_location, batch_size=args.batch_size, num_workers=args.workers)
+    print("setting up data loaders...")
 
+    # data_config = resolve_data_config(vars(args), model=base_model, verbose=True)
+    dataset_train = create_dataset(
+        'imagenet',
+        root=args.data_location,
+        split='train',
+        is_training=True,
+        class_map='',
+        download=False,
+        batch_size=args.batch_size,
+        seed=42,
+    )
+
+    dataset_eval = create_dataset(
+        'imagenet',
+        root=args.data_location,
+        split='validation',
+        is_training=False,
+        class_map='',
+        download=False,
+        batch_size=args.batch_size,
+    )
+    loader_train = create_loader(
+        dataset_train,
+        batch_size=args.batch_size,
+        input_size=(3, 224, 224),
+        is_training=True,
+        num_workers=args.workers,
+        device=device
+    )
+    loader_test = create_loader(
+        dataset_eval,
+        input_size=(3, 224, 224),
+        batch_size=args.batch_size,
+        is_training=False,
+        num_workers=args.workers,
+        device=device
+    )
+    
+    print("loading models...")
     sds = [torch.load(cp, map_location='cpu') for cp in model_paths]
-    feature_dim = sds[0]['classification_head.weight'].shape[1]
-    num_classes = sds[0]['classification_head.weight'].shape[0]
+    feature_dim = sds[0]['fc.weight'].shape[1]
+    num_classes = sds[0]['fc.weight'].shape[0]
     model = ModelWrapper(base_model, feature_dim, num_classes, normalize=True)
     model = model.to(device)
 
     _, names = make_functional(model)
     first = False
 
-    paramslist = [tuple(v.detach().requires_grad_().cpu() for _, v in sd.items()) for i, sd in enumerate(sds)]
+    paramslist = [tuple(v.detach().requires_grad_().cpu() for _, v in sd.items() if not isinstance(v, (torch.LongTensor, torch.cuda.LongTensor))) for i, sd in enumerate(sds)]
     torch.cuda.empty_cache()
     alpha_model = AlphaWrapper(paramslist, model, names)
 
@@ -182,11 +186,11 @@ if __name__ == '__main__':
     epochs = 5
 
     optimizer = torch.optim.AdamW(alpha_model.parameters(), lr=lr, weight_decay=0.)
-    num_batches = len(train_dset.train_loader)
+    num_batches = len(loader_train)
 
     for epoch in range(epochs):
         end = time.time()
-        for i, batch in enumerate(train_dset.train_loader):
+        for i, batch in enumerate(loader_train):
             step = i + epoch * num_batches
             batch = maybe_dictionarize_batch(batch)
             inputs, labels = batch['images'].cuda(), batch['labels'].cuda()
@@ -204,20 +208,20 @@ if __name__ == '__main__':
             optimizer.step()
 
             batch_time = time.time() - end
-            percent_complete = 100.0 * i / len(train_dset.train_loader)
+            percent_complete = 100.0 * i / len(loader_test)
             if ( i % 10 ) == 0:
-                # print(alpha_model.beta)
+                print(alpha_model.beta)
                 print(
-                    f"Train Epoch: {epoch} [{percent_complete:.0f}% {i}/{len(train_dset.train_loader)}]\t"
+                    f"Train Epoch: {epoch} [{percent_complete:.0f}% {i}/{num_batches}]\t"
                     f"Loss: {loss.item():.6f}\tData (t) {data_time:.3f}\tBatch (t) {batch_time:.3f}", flush=True
                 )
-                # print(alpha_model.alpha())
+                print(alpha_model.alpha())
             end = time.time()
 
-    acc = get_imagenet_acc(test_dset)
+    acc = get_imagenet_acc(loader_test)
     print('Accuracy is', 100 * acc)
-
-    # torch.save(
-    #     {'alpha' : alpha_model.alpha(), 'beta' : alpha_model.beta}, 
-    #     f'alphas_{lr}_{epochs}.pt'
-    # )
+    print(alpha_model.alpha())
+    torch.save(
+        {'alpha' : alpha_model.alpha(), 'beta' : alpha_model.beta}, 
+        f'alphas_{lr}_{epochs}.pt'
+    )
